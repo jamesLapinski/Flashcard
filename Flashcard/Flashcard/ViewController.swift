ab02c7091d07fc7d63910e0333ce146e4f079bc8
JLapinski <jlapin13@ford.com>
2018-07-20 10:57:43 -0400

    created project

---
+++ Flashcard/Flashcard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flashcard
+//
+//  Created by Lapinski, James Watson (J.) on 7/20/18.
+//  Copyright © 2018 Lapinski, James Watson (J.). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
